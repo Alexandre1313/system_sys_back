d3b02dbf13d60f63dc0c6e294f37c0d766707a65
Alexandre Cordeiro <alexandre.13a@gmail.com>
2024-12-07 16:18:21 -0300

    new pc

--- core/interfaces/index.ts
+++ core/interfaces/index.ts
@@ -1,63 +1,38 @@
-import Escola from "./Escola";
-import Estoque from "./Estoque";
-import Grade from "./Grade";
-import Item from "./Item";
-import Projeto from "./Projeto";
-import Tamanho from "./Tamanho";
-import Usuarios from "./Usuarios";
-import ItemTamanho from "./ItemTamanho";
-import GradeItem from "./GradeItem";
+import AddressCompany from "./AddressCompany";
 import Barcode from "./Barcode";
-import TamanhoQuantidade from "./TamanhoQuantidade";
-import DataInserction from "./DataInserction";
 import Caixa from "./Caixa";
 import CaixaItem from "./CaixaItem";
-import FinalyGrade from "./FinalyGrade";
-import ItensProjects from "./ItensProject";
-import ProjectItems from "./ProjectItems";
-import ProjetosSimp from "./ProjetosSimp";
-import OutInput from "./OutInput";
-import EntryInput from "./EntryInput";
+import Company from "./Company";
+import DataInserction from "./DataInserction";
+import DataInserctionUni from './DataInserctionUni';
 import Embalagem from "./Embalagem";
-import DataInserctionUni from './DataInserctionUni'
+import EntryInput from "./EntryInput";
+import Escola from "./Escola";
+import Estoque from "./Estoque";
+import FinalyGrade from "./FinalyGrade";
+import Grade from "./Grade";
+import GradeItem from "./GradeItem";
+import GradesRomaneio from "./GradesRomaneio";
+import Item from "./Item";
+import ItemsInserction from "./ItemsInserction";
+import ItemTamanho from "./ItemTamanho";
+import ItensProjects from "./ItensProject";
+import Login from "./Login";
+import OutInput from "./OutInput";
+import ProjectItems from "./ProjectItems";
+import Projeto from "./Projeto";
+import ProjetosSimp from "./ProjetosSimp";
 import QtyEmbDay from "./QtyEmbDay";
 import StockGenerate from "./StockGenerate";
-import AddressCompany  from "./AddressCompany";
+import Tamanho from "./Tamanho";
+import TamanhoQuantidade from "./TamanhoQuantidade";
 import TelephonesCompany from "./TelephonesCompany";
-import Company from "./Company";
-import GradesRomaneio from "./GradesRomaneio";
-import ItemsInserction from "./ItemsInserction";
-import Login from "./Login";
+import Usuarios from "./Usuarios";
 
-export type { 
-    Escola,
-    Estoque,  
-    Grade,
-    Item,
-    Projeto,
-    Tamanho,
-    Usuarios,
-    GradeItem,
-    ItemTamanho,
-    Barcode,
-    TamanhoQuantidade,
-    DataInserction,
-    Caixa, 
-    CaixaItem,
-    FinalyGrade,
-    ItensProjects,
-    ProjectItems,
-    ProjetosSimp,
-    OutInput,
-    EntryInput,
-    Embalagem,
-    DataInserctionUni,
-    QtyEmbDay,
-    StockGenerate,
-    AddressCompany,
-    Company,
-    TelephonesCompany,
-    GradesRomaneio,
-    ItemsInserction,
-    Login,
-}
+export type {
+    AddressCompany, Barcode, Caixa,
+    CaixaItem, Company, DataInserction, DataInserctionUni, Embalagem, EntryInput, Escola,
+    Estoque, FinalyGrade, Grade, GradeItem, GradesRomaneio, Item, ItemTamanho, ItemsInserction, ItensProjects, Login, OutInput, ProjectItems, Projeto, ProjetosSimp, QtyEmbDay,
+    StockGenerate, Tamanho, TamanhoQuantidade, TelephonesCompany, Usuarios
+};
+
