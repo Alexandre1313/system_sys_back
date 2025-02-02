010eb60afde99a891f1d9479b8f91996b827a467
Alexandre Cordeiro <alexandre.13a@gmail.com>
2025-02-02 15:19:46 -0300

    020220255

--- prisma/seed2.ts
+++ prisma/seed2.ts
@@ -63,9 +63,9 @@ async function seed2() {
                         data: {
                             escolaId: escola.id,
                             finalizada: false,
-                            companyId: 2,
-                            createdAt: "2025-01-28T19:39:00.739Z", 
-                            updatedAt: "2025-01-28T19:39:00.739Z" 
+                            companyId: 999992,
+                            //createdAt: "2025-01-30T19:39:00.739Z", 
+                            //updatedAt: "2025-01-30T19:39:00.739Z" 
                         },
                     });
                     console.log(`Grade (ID: ${grade.id}) criada.`);
