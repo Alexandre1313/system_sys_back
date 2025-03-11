6539cb92f78856aaa60e95f39cb0b9a6045bff08
Alexandre Cordeiro <alexandre.13a@gmail.com>
2025-03-11 16:05:47 -0300

    110320251

--- prisma/seed2.ts
+++ prisma/seed2.ts
@@ -63,7 +63,7 @@ async function seed2() {
                         data: {
                             remessa: 1,
                             escolaId: escola.id,                          
-                            companyId: 266666,
+                            companyId: 23333,
                             //createdAt: "2025-02-17T11:39:00.739Z", 
                             //updatedAt: "2025-02-17T11:39:00.739Z", 
                             //tipo: "REPOSIÇÂO",
