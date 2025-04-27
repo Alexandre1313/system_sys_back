ff4dff9ebc37421ff425c9a69263c1e789d8bec2
Alexandre1313 <seu@email.com>
2025-04-27 18:50:08 -0300

    270420251

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(4997, "0.0.0.0");
+  await app.listen(4997, '192.168.1.169');
 }
 
 bootstrap();
