032120c29fd1570b7bcc306d8b23a32c3e7ab457
Alexandre Cordeiro <alexandre.13a@gmail.com>
2025-08-12 16:57:38 -0300

    install ubunto120820252 to service

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true, logger: ['error', 'warn'], });
-  await app.listen(4997, '192.168.1.169', () => console.log('\x1b[32m%s\x1b[0m', '✅ Server running on IP 192.168.1.169 and listening on port 4997'));
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['error', 'warn'],
+  });
+
+  const isProd = process.env.NODE_ENV === 'production';
+  const port = isProd ? 4997 : 4999;
+  const host = '192.168.1.169';
+
+  await app.listen(port, host, () => {
+    console.log('\x1b[32m%s\x1b[0m', `✅ Server running on IP ${host} and listening on port ${port} (${isProd ? 'PROD' : 'DEV'})`);
+  });
 }
 
 bootstrap();
